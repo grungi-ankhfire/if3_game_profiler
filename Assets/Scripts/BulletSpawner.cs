19ef6dcda28c8385663828b20236a0a4aa645019
Bastien Gorissen <kadomony@gmail.com>
2020-12-02 18:31:46 +0100

    Added unity project and initial files

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float movementAmplitude;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().position = new Vector3(0, movementAmplitude * Mathf.Sin(Time.time), 0);
+        GetComponent<Transform>().Rotate(new Vector3(0,0, rotationSpeed * Time.deltaTime));
+
+
+        Instantiate(bulletPrefab, GetComponent<Transform>().position + 0.5f * GetComponent<Transform>().right,  GetComponent<Transform>().rotation);
+
+    }
+}
